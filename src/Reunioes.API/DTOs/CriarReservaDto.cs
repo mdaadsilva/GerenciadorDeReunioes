9dd6984c9d6d24eb0a20fbd52e02666f4dc97988
Martin de Almeida Alves da Silva <martindealmeida17@gmail.com>
2025-07-11 16:01:04 -0300

    feat: projeto final

---
+++ src/Reunioes.API/DTOs/CriarReservaDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Reunioes.API.DTOs
+{
+    public class CriarReservaDto
+    {
+        public required DateTime Inicio { get; set; }
+        public required DateTime Fim { get; set; }
+        public required int SalaId { get; set; }
+    }
+}
---
+++ src/Reunioes.API/Models/Sala.cs
@@ -0,0 +1,10 @@
+﻿namespace Reunioes.API.Models
+{
+    public class Sala
+    {
+        public virtual int Id { get; protected set; }
+        public required virtual string Nome { get; set; }
+        public required virtual int Andar { get; set; }
+        public required virtual int QuantidadeAssentos { get; set; }
+    }
+}
